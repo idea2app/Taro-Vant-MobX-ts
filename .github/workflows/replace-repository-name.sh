bc79a391bd0d2ebc90ad0e0a24f71a4473fae6eb
TechQuery <shiy2008@gmail.com>
2024-10-27 18:12:42 +0800

    [optimize] upgrade to Node.js 20, Taro 4, KoAJAX 3 & other latest Upstream packages/actions [fix] Initialization scripts of Template Repository

--- .github/workflows/replace-repository-name.sh
+++ .github/workflows/replace-repository-name.sh
@@ -7,5 +7,7 @@ TargetName=$4
 
 sed -i "s/$SourceOwner\/$SourceName/$TargetOwner\/$TargetName/ig" \
     $(grep -i $SourceOwner/$SourceName -rl . --exclude-dir=.git --exclude-dir=node_modules)
+sed -i "s/$SourceOwner/$TargetOwner/ig" \
+    $(grep -i $SourceOwner -rl . --exclude-dir=.git --exclude-dir=node_modules)
 sed -i "s/$SourceName/$TargetName/ig" \
     $(grep -i $SourceName -rl . --exclude-dir=.git --exclude-dir=node_modules)
