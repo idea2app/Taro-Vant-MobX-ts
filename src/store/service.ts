461c6769337c14bb5c9636d3127bbea882f18155
TechQuery <shiy2008@gmail.com>
2025-03-22 04:03:52 +0800

    [migrate] replace Custom Request method with KoAJAX Taro adapter

--- src/store/service.ts
+++ src/store/service.ts
@@ -1,44 +1,16 @@
-import { request, showModal } from '@tarojs/taro';
 import { Request } from 'koajax';
+import { request as baseRequest } from 'koajax-taro-adapter';
 import { githubClient } from 'mobx-github';
 
 if (typeof fetch !== 'function') {
   githubClient.baseURI = 'https://oss-toolbox.kaiyuanshe.cn/api/GitHub/';
 
-  githubClient.baseRequest = function <B>({
-    method,
-    path,
-    headers,
-    body
-  }: Request<B>) {
-    const { token } = this;
+  githubClient.baseRequest = function <B>({ path, ...option }: Request<B>) {
+    const { pathname, search } = new URL(path, this.baseURI);
 
-    const response = request({
-      method,
-      url: new URL(path, this.baseURI) + '',
-      header: token
-        ? { ...headers, Authorization: `Bearer ${token}` }
-        : headers,
-      mode: 'cors',
-      data: body
-    }).then(({ statusCode, errMsg, header, data }) => {
-      if (statusCode < 300)
-        return {
-          status: statusCode,
-          statusText: errMsg,
-          headers: header,
-          body: data
-        };
-      const { message } = data;
-
-      if (method !== 'GET' || path !== 'session' || statusCode !== 401)
-        showModal({ title: statusCode === 401 ? '需要登录' : message });
-
-      throw new URIError(message);
+    return baseRequest({
+      ...option,
+      path: `${this.baseURI}${pathname.slice(1)}${search}`
     });
-
-    async function* progress() {}
-
-    return { response, download: progress() };
   };
 }
