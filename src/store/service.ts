cdf0b9f89bc3ec838f7ae4cdc5037c8d1ea62446
TechQuery <shiy2008@gmail.com>
2025-03-21 18:10:53 +0800

    [revert] old KoAJAX request method for new Adapter API [fix] Taro compatibility bug of AntmJS VantUI (https://github.com/AntmJS/vantui/issues/715#issuecomment-2652550807) [optimize] update Upstream packages

--- src/store/service.ts
+++ src/store/service.ts
@@ -1,7 +1,44 @@
-import { request } from 'koajax-taro-adapter';
+import { request, showModal } from '@tarojs/taro';
+import { Request } from 'koajax';
 import { githubClient } from 'mobx-github';
 
 if (typeof fetch !== 'function') {
-  githubClient.baseRequest = request;
   githubClient.baseURI = 'https://oss-toolbox.kaiyuanshe.cn/api/GitHub/';
+
+  githubClient.baseRequest = function <B>({
+    method,
+    path,
+    headers,
+    body
+  }: Request<B>) {
+    const { token } = this;
+
+    const response = request({
+      method,
+      url: new URL(path, this.baseURI) + '',
+      header: token
+        ? { ...headers, Authorization: `Bearer ${token}` }
+        : headers,
+      mode: 'cors',
+      data: body
+    }).then(({ statusCode, errMsg, header, data }) => {
+      if (statusCode < 300)
+        return {
+          status: statusCode,
+          statusText: errMsg,
+          headers: header,
+          body: data
+        };
+      const { message } = data;
+
+      if (method !== 'GET' || path !== 'session' || statusCode !== 401)
+        showModal({ title: statusCode === 401 ? '需要登录' : message });
+
+      throw new URIError(message);
+    });
+
+    async function* progress() {}
+
+    return { response, download: progress() };
+  };
 }
