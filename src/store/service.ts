7bd3df04e1e6b9027ffaaedeceb64077621923f7
TechQuery <shiy2008@gmail.com>
2026-03-26 02:24:45 +0800

    [migrate] upgrade to KoAJAX 3.3, Node.js 24, Globals 17, ESLint 10 & other latest Upstream packages/actions

--- src/store/service.ts
+++ src/store/service.ts
@@ -1,7 +1,21 @@
-import { Request } from 'koajax';
-import { request } from 'koajax-taro-adapter';
+import { Event, EventTarget } from 'event-target-shim';
+import { defaultHTTPRuntime, HTTPToolkit, Request } from 'koajax';
 import { githubClient } from 'mobx-github';
+import { Blob, fetch, Headers, ReadableStream } from 'taro-fetch-polyfill';
 
+const { request } = new HTTPToolkit({
+  ...defaultHTTPRuntime,
+  Event: Event as typeof globalThis.Event,
+  EventTarget: EventTarget as typeof globalThis.EventTarget,
+  Headers,
+  Blob,
+  ReadableStream,
+  fetch: fetch as typeof globalThis.fetch
+});
+
+/**
+ * @see {@link https://github.com/NervJS/taro/pull/17472}
+ */
 export function baseRequest<B>({ path, ...option }: Request<B>) {
   const { pathname, search } = new URL(path, this.baseURI);
 
